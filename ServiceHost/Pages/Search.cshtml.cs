5b0093baeaa2af142b84ad48230f6809729fef2c
Ali <pachinco.a.m@gmail.com>
2023-04-06 18:05:52 +0330

    Event search implemented

---
+++ ServiceHost/Pages/Search.cshtml.cs
@@ -0,0 +1,24 @@
+using _01_EventiQuery.Contracts.Event;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceHost.Pages
+{
+    public class SearchModel : PageModel
+    {
+        public string Value;
+        public List<EventQueryModel> Events;
+        private readonly IEventQuery _eventQuery;
+
+        public SearchModel(IEventQuery eventQuery)
+        {
+            _eventQuery = eventQuery;
+        }
+
+        public async Task OnGet(string value)
+        {
+            Value = value;
+            Events = await _eventQuery.SearchAsync(value);
+        }
+    }
+}
